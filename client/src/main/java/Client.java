ec7025aee7dd8748082c395497d92fd6eec55a00
kimullamen <pbrehpuum@gmail.com>
2016-12-05 08:11:33 +0900

    クライアントを追加

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -14,6 +14,13 @@ import java.net.Socket;
 @Slf4j
 public class Client {
     public static void main(String[] args) {
+        perLine();
+    }
+
+    /**
+     * サーバへ1行送信するたびに受信を待つ
+     */
+    public static void perLine() {
         try (
                 Socket socket = new Socket("localhost", 8080);
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
@@ -35,6 +42,39 @@ public class Client {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
+    /**
+     * サーバへの送信がすべて完了してから受信を待つ
+     */
+    public static void batch() {
+        try {
+            try (
+                    Socket socket = new Socket("localhost", 8080);
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    BufferedReader keyIn = new BufferedReader(new InputStreamReader(System.in));
+            ) {
+                log.info("サーバに接続しました " + socket.getLocalSocketAddress() + " to " + socket.getRemoteSocketAddress());
+
+                String input;
+                while (!(input = keyIn.readLine()).equals("enter")) {
+                    out.write(input);
+                }
+                out.close();
+
+                while (true) {
+                    input = in.readLine();
+                    if (input == null) {
+                        break;
+                    }
+                    log.info(input);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
