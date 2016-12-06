80f87cb7c98154ddf9342021b3e8297ce78db206
kimullamen <pbrehpuum@gmail.com>
2016-12-06 22:12:43 +0900

    AsyncContextのサンプル追加

--- async-context/src/main/java/example/com/echo/EchoServlet.java
+++ async-context/src/main/java/example/com/echo/EchoServlet.java
@@ -1,5 +1,8 @@
 package example.com.echo;
 
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,16 +10,48 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-@WebServlet(urlPatterns = "/")
+@Slf4j
+@WebServlet(urlPatterns = "/", asyncSupported = true)
 public class EchoServlet extends HttpServlet {
+    Executor executor = new ScheduledThreadPoolExecutor(10);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.println("hello world");
-        writer.flush();
-        writer.close();
+        log.info("begin doGet");
+        AsyncContext ac = req.startAsync();
+        executor.execute(new SlowTask(ac));
+        log.info("end doGet");
+    }
+}
+
+@Slf4j
+class SlowTask implements Runnable {
+    private AsyncContext ac;
+
+    SlowTask(AsyncContext ac) {
+        this.ac = ac;
     }
 
+    @Override
+    public void run() {
+        log.info("begin AsyncContext#start");
+
+        try {
+            PrintWriter writer = ac.getResponse().getWriter();
+            for (int i = 0; i < 5; i++) {
+                writer.println("task :" + i);
+                writer.flush();
+                log.info("send chuncked data");
+                TimeUnit.SECONDS.sleep(2);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        ac.complete();
+        log.info("end AsyncContext#start");
+    }
 }
